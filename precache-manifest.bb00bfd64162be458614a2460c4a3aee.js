55e9441dc46593cff59be7d9ebaf55a8a49c4140
Anton Aduisky <a.mltw@yandex.ru>
2023-05-22 21:05:33 +0300

    Updates

---
+++ precache-manifest.bb00bfd64162be458614a2460c4a3aee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef8716dcd233dabe3d9c0012c4dfef7",
+    "url": "/fgbnu_task/index.html"
+  },
+  {
+    "revision": "7029da0562f18785a37c",
+    "url": "/fgbnu_task/static/css/main.7e3ca00e.chunk.css"
+  },
+  {
+    "revision": "91625cdcf21f3f6471d0",
+    "url": "/fgbnu_task/static/js/2.e73f3334.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/fgbnu_task/static/js/2.e73f3334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7029da0562f18785a37c",
+    "url": "/fgbnu_task/static/js/main.d1f573ca.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/fgbnu_task/static/js/main.d1f573ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ac8736f4ac49792d86",
+    "url": "/fgbnu_task/static/js/runtime-main.570fb55c.js"
+  }
+]);
